d44d9085cdc2c639a340485b5e36d93924023436
tsuchinokoReal <parkermorgan@rocketmail.com>
2023-12-06 17:58:32 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CS 230L</h1>
+	<h2>Section - 03</h2>
+	<p>800323506</p>
+	<p>Hi I am Parker Morgan</p>
+    </div>
+  );
+}
+
+export default App;
