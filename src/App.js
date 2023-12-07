c94df768bffd2ee096c120c8acdd9a8cf1318e17
tsuchinokoReal <parkermorgan@rocketmail.com>
2023-12-06 20:02:15 -0500

    third part

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import './index.js';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import  "./Card.js";
+import  "./Home.js";
+import  "./Contact.js";
 
-const naviBar = document.createElement('navi-bar');
-
-document.body.appendChild(naviBar);
 function App() {
   return (
     <div className="App">
-	<navi-bar/>
-      <h1>CS 230L</h1>
-	<h2>Section - 03</h2>
-	<p>800323506</p>
-	<p>Hi I am Parker Morgan</p>
-<custom-card title="card1"/> <custom-card title="card2" color="bg-secondary"/><custom-card title="card3" color="bg-danger"/>
+	<BrowserRouter>
+		<navi-bar/>
+		<Routes>
+		<Route path="/" element={<my-home />}/>
+		<Route path="/card" element={<custom-card />} /> 
+      	<Route path="contact" element={<contact-me />} /> 
+       </Routes>
+	</BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -24,26 +24,11 @@ class NaviBar extends HTMLElement {
     shadow.appendChild(div);
   }
 }
-class CustomCard extends HTMLElement {
-  connectedCallback() {
-	const title = this.getAttribute('title') || '';
-	const text = this.getAttribute('text') || '';
-	const color = this.getAttribute('color') || 'bg-primary';
-	const header = this.getAttribute('header') || '';
 
-	this.innerHTML = `
-      <div class="card text-white ${color} mb-3" style="max-width: 18rem; margin-right: 10px;">
-        <div class="card-header">${header}</div>
-        <div class="card-body">
-          <h5 class="card-title">${title}</h5>
-          <p class="card-text">${text}</p>
-        </div>
-      </div>
-    `;
-  }
-}
 
-customElements.define('custom-card', CustomCard);
+
+
+
 customElements.define('navi-bar', NaviBar);
 
 // If you want to start measuring performance in your app, pass a function
