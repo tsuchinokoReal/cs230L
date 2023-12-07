ebe979e4c4c455f410cdbd65ee0e743f007ac151
tsuchinokoReal <parkermorgan@rocketmail.com>
2023-12-06 19:20:30 -0500

    second part

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import './index.js';
 
+const naviBar = document.createElement('navi-bar');
+
+document.body.appendChild(naviBar);
 function App() {
   return (
     <div className="App">
+	<navi-bar/>
       <h1>CS 230L</h1>
 	<h2>Section - 03</h2>
 	<p>800323506</p>
 	<p>Hi I am Parker Morgan</p>
+<custom-card title="card1"/> <custom-card title="card2" color="bg-secondary"/><custom-card title="card3" color="bg-danger"/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "bootstrap/dist/css/bootstrap.css";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -10,6 +11,40 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+class NaviBar extends HTMLElement {
+  constructor() {
+    super();
+
+    const shadow = this.attachShadow({ mode: 'open' });
+
+    const div = document.createElement('div');
+    div.innerHTML = '<nav class="navbar navbar-light bg-light"> <div class="container-fluid"> <a class="navbar-brand">Navbar</a> <form class="d-flex"> <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/> <button class="btn btn-outline-success" type="submit">Search</button></form> </div></nav>';
+
+    shadow.appendChild(div);
+  }
+}
+class CustomCard extends HTMLElement {
+  connectedCallback() {
+	const title = this.getAttribute('title') || '';
+	const text = this.getAttribute('text') || '';
+	const color = this.getAttribute('color') || 'bg-primary';
+	const header = this.getAttribute('header') || '';
+
+	this.innerHTML = `
+      <div class="card text-white ${color} mb-3" style="max-width: 18rem; margin-right: 10px;">
+        <div class="card-header">${header}</div>
+        <div class="card-body">
+          <h5 class="card-title">${title}</h5>
+          <p class="card-text">${text}</p>
+        </div>
+      </div>
+    `;
+  }
+}
+
+customElements.define('custom-card', CustomCard);
+customElements.define('navi-bar', NaviBar);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
